8ac670ca88929b60fc040386388008faaa43c811
Meoso <meoso@users.noreply.github.com>
2014-09-09 13:11:43 -0500

    modified:   show-uprecord.sh

--- show-uprecord.sh
+++ show-uprecord.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-uprecord=$(<~/.uprecord)
-uprecord=${uprecord%%.*}
-#uprecordseconds=$(( uprecord%60 ))
-uprecordminutes=$(( uprecord/60%60 ))
-uprecordhours=$(( uprecord/60/60%24 ))
-uprecorddays=$(( uprecord/60/60/24 ))
+if [ -f ~/.uprecord ] 
+then
+	uprecord=$(<~/.uprecord)
+	uprecord=${uprecord%%.*}
+	#uprecordseconds=$(( uprecord%60 ))
+	uprecordminutes=$(( uprecord/60%60 ))
+	uprecordhours=$(( uprecord/60/60%24 ))
+	uprecorddays=$(( uprecord/60/60/24 ))
 
-echo "${uprecorddays}d ${uprecordhours}h ${uprecordminutes}m"
+	echo "${uprecorddays}d ${uprecordhours}h ${uprecordminutes}m"
+else
+	echo "no record"
+fi
